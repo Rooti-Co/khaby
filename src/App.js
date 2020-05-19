9fa6e389dd52d195884e508696952be5fc136244
Andrii <48836383+Sizoy@users.noreply.github.com>
2020-05-19 19:07:58 +0300

    header is ready!!!!!!

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "./components/Header";
 
 function App() {
-    return <div>Nothing there</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,169 @@
+import React, { useState } from "react";
+import "./Header.css";
+import { useMediaQuery } from "react-responsive";
+import headerLogo from "./images/header_logo.png";
+import headerHeart from "./images/header_heart.svg";
+import headerSearch from "./images/header_search.svg";
+import headerSearchMobile from "./images/header_search_mobile.svg";
+import headerLogoMobile from "./images/header_logo_mobile.png";
+import headerBurger from "./images/header_burger.svg";
+import headerCloseMobile from "./images/header_close_mobile.svg";
+
+const Header = (props) => {
+  const [menuOpened, setMenuOpened] = useState(false);
+  const [subMenuOpened, setSubMenuOpened] = useState(false);
+  const [searchOpened, setSearchOpened] = useState(false);
+  const isMobile = useMediaQuery({ query: "(max-width: 940px)" });
+
+  return (
+    <header className="header">
+      <div className="header-container">
+        {/* BURGER */}
+        {isMobile && (
+          <button
+            onClick={() => {
+              setMenuOpened(true);
+            }}
+          >
+            <img src={headerBurger} alt="menu" />
+          </button>
+        )}
+
+        {/* LOGO */}
+        <a href="#" className="header__logo">
+          <img src={isMobile ? headerLogoMobile : headerLogo} alt="Khaby" />
+        </a>
+
+        {/* NAVIGTION */}
+        <nav
+          className={
+            !isMobile
+              ? "header-menu"
+              : !menuOpened
+              ? "header-menu"
+              : "header-menu opened"
+          }
+        >
+          {!subMenuOpened && isMobile && (
+            <button
+              onClick={() => {
+                setMenuOpened(false);
+              }}
+              className="header-menu-list-mobile__close"
+            >
+              <img src={headerCloseMobile} alt="close" />
+            </button>
+          )}
+          <ul className="header-menu-list">
+            <li
+              onClick={() => {
+                if (!subMenuOpened) {
+                  setSubMenuOpened(true);
+                }
+              }}
+            >
+              <a href="#" className="header-menu-list__item">
+                каталог
+              </a>
+
+              <ul
+                className={
+                  !isMobile
+                    ? "header-menu-list-dropdown"
+                    : !subMenuOpened
+                    ? "header-menu-list-dropdown-mobile"
+                    : "header-menu-list-dropdown-mobile opened"
+                }
+              >
+                {subMenuOpened && isMobile && (
+                  <button
+                    onClick={() => {
+                      setSubMenuOpened(false);
+                    }}
+                    className="header-menu-list-dropdown-mobile__close"
+                  >
+                    <img src={headerCloseMobile} alt="close" />
+                  </button>
+                )}
+                <li className="header-menu-list-dropdown__item">
+                  <a href="#">штани</a>
+                </li>
+                <li className="header-menu-list-dropdown__item">
+                  <a href="#">сорочки</a>
+                </li>
+                <li className="header-menu-list-dropdown__item">
+                  <a href="#">сукні</a>
+                </li>
+                <li className="header-menu-list-dropdown__item">
+                  <a href="#">сведри</a>
+                </li>
+                <li className="header-menu-list-dropdown__item">
+                  <a href="#">костюми</a>
+                </li>
+                <li className="header-menu-list-dropdown__item">
+                  <a href="#">піджаки</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="#" className="header-menu-list__item">
+                про нас
+              </a>
+            </li>
+            <li>
+              <a href="#" className="header-menu-list__item">
+                оплата і доставка
+              </a>
+            </li>
+            <li>
+              <a href="#" className="header-menu-list__item">
+                lookbook
+              </a>
+            </li>
+            <li>
+              <a href="#" className="header-menu-list__item">
+                контакти
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="header-right-block">
+          {/* LIKES */}
+          <button className="header-right-block__likes">
+            <div className="header-right-block__likes-counter">1</div>
+            <img src={headerHeart} alt="like" />
+          </button>
+
+          {/* SEARCH */}
+          {!isMobile ? (
+            <div className="header-right-block__search">
+              <a href="#">
+                <img src={headerSearch} alt="search" />
+              </a>
+              <input type="text" placeholder="Пошук" />
+            </div>
+          ) : (
+            <button
+              onClick={() => {
+                setSearchOpened(!searchOpened);
+              }}
+            >
+              <img src={headerSearchMobile} alt="search" />
+            </button>
+          )}
+        </div>
+        {isMobile && searchOpened && (
+          <div className="header-right-block__search">
+            <a href="#">
+              <img src={headerSearch} alt="search" />
+            </a>
+            <input type="text" placeholder="Пошук" />
+          </div>
+        )}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
