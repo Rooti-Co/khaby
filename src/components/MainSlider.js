883d8381be7645011817b55288499029cfcf41da
Andrew <55840012+marchukandrij@users.noreply.github.com>
2020-05-19 21:52:46 +0300

    join W030 Main Slider

--- src/components/MainSlider.js
+++ src/components/MainSlider.js
@@ -1,47 +1,75 @@
 import React from "react";
-import "../styles/MainSlider.css";
+import "./mainSliderAssets/MainSlider.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
-  export default class MainSlider extends React.Component {
+export default class MainSlider extends React.Component {
     render() {
-      const settings = {
-        infinite: true,
-        speed: 1200,
-        autoplaySpeed: 5500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        pauseOnHover: false
-      };
-      return (
-        <div>
-          <Slider {...settings}
-          className="main-slider container"
-          >
+        const settings = {
+            infinite: true,
+            speed: 1200,
+            autoplaySpeed: 5500,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            pauseOnHover: false,
+        };
+        return (
             <div>
-              <img className="main-slider__item" src="/main-slider-item1.jpg" alt="slider pic"/>
-              <div className="main-slider__slide-text">Обирай свій мінімалізм</div>
+                <Slider {...settings} className="main-slider container">
+                    <div>
+                        <img
+                            className="main-slider__item"
+                            src="/main-slider-item1.jpg"
+                            alt="slider pic"
+                        />
+                        <div className="main-slider__slide-text">
+                            Обирай свій мінімалізм
+                        </div>
+                    </div>
+                    <div>
+                        <img
+                            className="main-slider__item"
+                            src="/main-slider-item1.jpg"
+                            alt="slider pic"
+                        />
+                        <div className="main-slider__slide-text">
+                            Обирай свій мінімалізм
+                        </div>
+                    </div>
+                    <div>
+                        <img
+                            className="main-slider__item"
+                            src="/main-slider-item1.jpg"
+                            alt="slider pic"
+                        />
+                        <div className="main-slider__slide-text">
+                            Обирай свій мінімалізм
+                        </div>
+                    </div>
+                    <div>
+                        <img
+                            className="main-slider__item"
+                            src="/main-slider-item1.jpg"
+                            alt="slider pic"
+                        />
+                        <div className="main-slider__slide-text">
+                            Обирай свій мінімалізм
+                        </div>
+                    </div>
+                    <div>
+                        <img
+                            className="main-slider__item"
+                            src="/main-slider-item1.jpg"
+                            alt="slider pic"
+                        />
+                        <div className="main-slider__slide-text">
+                            Обирай свій мінімалізм
+                        </div>
+                    </div>
+                </Slider>
             </div>
-            <div>
-              <img className="main-slider__item" src="/main-slider-item1.jpg" alt="slider pic"/>
-              <div className="main-slider__slide-text">Обирай свій мінімалізм</div>
-            </div>
-            <div>
-              <img className="main-slider__item" src="/main-slider-item1.jpg" alt="slider pic"/>
-              <div className="main-slider__slide-text">Обирай свій мінімалізм</div>
-            </div>
-            <div>
-              <img className="main-slider__item" src="/main-slider-item1.jpg" alt="slider pic"/>
-              <div className="main-slider__slide-text">Обирай свій мінімалізм</div>
-            </div>
-            <div>
-              <img className="main-slider__item" src="/main-slider-item1.jpg" alt="slider pic"/>
-              <div className="main-slider__slide-text">Обирай свій мінімалізм</div>
-            </div>
-          </Slider>
-        </div>
-      );
+        );
     }
-  }
+}
