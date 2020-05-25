9d25875eeeb080830451ec27ab95735fc48c41fd
Pozakhmarnyi <57231356+Pozakhmarnyi@users.noreply.github.com>
2020-05-25 18:54:46 +0300

    work done

--- src/App.js
+++ src/App.js
@@ -1,130 +1,123 @@
 import React from "react";
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-    useParams,
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
 } from "react-router-dom";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
-import PageAboutUs from "./components/pageAboutUs";
+// import Footer from "./components/Footer";
+import { PageAboutUs } from "./components/pageAboutUs";
 import PageLookbookSlider from "./components/pageLookbook";
 import MainSlider from "./components/MainSlider";
 import Feedback from "./components/feedback";
 import Catalog from "./components/catalog";
 import {
-    getHomePageUrl,
-    getMainMenuData,
-    getCategoriesMenuData,
-    getFavoritePageUrl,
-    getSearchPageUrl,
+  getHomePageUrl,
+  getMainMenuData,
+  getCategoriesMenuData,
+  getFavoritePageUrl,
+  getSearchPageUrl,
 } from "./components/configuration";
 
 import { LoadedPage } from "./components/demoLoadedPage";
 
 // Component for display catalog in different categories
 const CatalogNavigator = () => {
-    let { category } = useParams();
-    return (
-        <>
-            {!category && <Catalog type="all" />}
+  let { category } = useParams();
+  return (
+    <>
+      {!category && <Catalog type="all" />}
 
-            {category && (
-                <div className="page_decorator">
-                    Insert here Catalog Page Component. Render Category{" "}
-                    {category}
-                    <Catalog type="shirts" />
-                </div>
-            )}
-        </>
-    );
+      {category && (
+        <div className="page_decorator">
+          Insert here Catalog Page Component. Render Category {category}
+          <Catalog type="shirts" />
+        </div>
+      )}
+    </>
+  );
 };
 
 // Component for display product by ID
 const ProductNavigator = () => {
-    let { product_id } = useParams();
-    return (
-        <div className="page_decorator">
-            Insert here Product Page Component. Render ID {product_id}
-        </div>
-    );
+  let { product_id } = useParams();
+  return (
+    <div className="page_decorator">
+      Insert here Product Page Component. Render ID {product_id}
+    </div>
+  );
 };
 
 function App() {
-    return (
-        <Router>
-            {/* This is demo navigation Buttons. Remove later */}
-            <div className="temp_decorator">
-                <Link className="link_decorator" to={getHomePageUrl()}>
-                    Головна
-                </Link>
-                {getMainMenuData().map((item, index) => (
-                    <Link className="link_decorator" to={item.url} key={index}>
-                        {item.title}
-                    </Link>
-                ))}
-                {getCategoriesMenuData().map((item, index) => (
-                    <Link className="link_decorator" to={item.url} key={index}>
-                        {item.title}
-                    </Link>
-                ))}
-                <Link className="link_decorator" to={getFavoritePageUrl()}>
-                    Вибране
-                </Link>
-                <Link className="link_decorator" to={getSearchPageUrl()}>
-                    Пошук
-                </Link>
-            </div>
+  return (
+    <Router>
+      {/* This is demo navigation Buttons. Remove later */}
+      <div className="temp_decorator">
+        <Link className="link_decorator" to={getHomePageUrl()}>
+          Головна
+        </Link>
+        {getMainMenuData().map((item, index) => (
+          <Link className="link_decorator" to={item.url} key={index}>
+            {item.title}
+          </Link>
+        ))}
+        {getCategoriesMenuData().map((item, index) => (
+          <Link className="link_decorator" to={item.url} key={index}>
+            {item.title}
+          </Link>
+        ))}
+        <Link className="link_decorator" to={getFavoritePageUrl()}>
+          Вибране
+        </Link>
+        <Link className="link_decorator" to={getSearchPageUrl()}>
+          Пошук
+        </Link>
+      </div>
 
-            {/* ============ This is Main Application ============ */}
-            <Header />
+      {/* ============ This is Main Application ============ */}
+      <Header />
 
-            <Switch>
-                <Route
-                    path="/catalog/:category"
-                    children={<CatalogNavigator />}
-                />
-                <Route path="/catalog" children={<CatalogNavigator />} />
-                <Route
-                    path="/products/:product_id"
-                    children={<ProductNavigator />}
-                />
-                <Route path="/favorite">
-                    <div className="page_decorator">
-                        Insert here Favorite Page Component
-                    </div>
-                </Route>
-                <Route path="/about">
-                    <PageAboutUs />
-                </Route>
-                <Route path="/lookbook">
-                    <PageLookbookSlider />
-                </Route>
-                <Route path="/contact">
-                    <div className="page_decorator">
-                        Insert here Contact Page Component
-                    </div>
-                </Route>
-                <Route path="/order_and_delivery">
-                    <LoadedPage />
-                </Route>
-                <Route path="/search">
-                    <div className="page_decorator">
-                        Insert here Search Page Component
-                    </div>
-                </Route>
-                <Route path="/">
-                    <>
-                        <MainSlider />
-                        <Catalog type="main" />
-                        <Feedback />
-                    </>
-                </Route>
-            </Switch>
+      <Switch>
+        <Route path="/catalog/:category" children={<CatalogNavigator />} />
+        <Route path="/catalog" children={<CatalogNavigator />} />
+        <Route path="/products/:product_id" children={<ProductNavigator />} />
+        <Route path="/favorite">
+          <div className="page_decorator">
+            Insert here Favorite Page Component
+          </div>
+        </Route>
+        <Route path="/about">
+          <PageAboutUs />
+        </Route>
+        <Route path="/lookbook">
+          <PageLookbookSlider />
+        </Route>
+        <Route path="/contact">
+          <div className="page_decorator">
+            Insert here Contact Page Component
+          </div>
+        </Route>
+        <Route path="/order_and_delivery">
+          <LoadedPage />
+        </Route>
+        <Route path="/search">
+          <div className="page_decorator">
+            Insert here Search Page Component
+          </div>
+        </Route>
+        <Route path="/">
+          <>
+            <MainSlider />
+            <Catalog type="main" />
+            <Feedback />
+          </>
+        </Route>
+      </Switch>
 
-            <Footer />
-        </Router>
-    );
+      {/* <Footer /> */}
+    </Router>
+  );
 }
 export default App;
--- src/components/pageAboutUs.js
+++ src/components/pageAboutUs.js
@@ -1,40 +1,86 @@
 import React from "react";
 import "./pageAboutUsAssets/AboutUs.css";
+import { useState } from "react";
+import { wordPressAPIUrl } from "./configuration";
 
-export default function PageAboutUs() {
-    return (
-        <div className="about-us container">
-            <div className="about-us__image-box">
-                <img
-                    className="about-us__image"
-                    src="/about-us-img.jpg"
-                    alt="About us"
-                />
-            </div>
+// export default function PageAboutUs() {
+//   return (
+//     <div className="about-us container">
+//       <div className="about-us__image-box">
+//         <img
+//           className="about-us__image"
+//           src="/about-us-img.jpg"
+//           alt="About us"
+//         />
+//       </div>
 
-            <div className="about-us__info-box">
-                <h3 className="about-us__title">Про нас</h3>
-                <div className="about-us__text">
-                    Khaby це молодий український бренд базового жіночого одягу.
-                    Концепція простоти та мінімалізму продумана і реалізована
-                    засновником і дизайнером марки Іриною ще на початку
-                    створення. Ідея розробки кежуал моделей з дизайнерськими
-                    акцентами становить філософію бренду, який набуває
-                    популярності серед своєї категорії людей.
-                </div>
-                <div className="about-us__text-italic">
-                    Окрема увага приділяється якості: всі моделі бренду Khaby
-                    виготовляються з якісних тканин на українських фабриках.
-                </div>
-                <div className="about-us__text">
-                    Ключовими моделями колекцій Khaby є брючні костюми, сорочки,
-                    спідниці та брюки з бездоганною посадкою, а також сукні та
-                    светри, які вписуються в контекст будь-якого гардеробу і,
-                    зберігають свою актуальність протягом кількох сезонів.
-                    <br /> Наша ідея - допомогти дівчатам створити практичний і
-                    продуманий базовий гардероб!
-                </div>
-            </div>
-        </div>
-    );
+//       <div className="about-us__info-box">
+//         <h3 className="about-us__title">Про нас</h3>
+//         <div className="about-us__text">
+//           Khaby це молодий український бренд базового жіночого одягу. Концепція
+//           простоти та мінімалізму продумана і реалізована засновником і
+//           дизайнером марки Іриною ще на початку створення. Ідея розробки кежуал
+//           моделей з дизайнерськими акцентами становить філософію бренду, який
+//           набуває популярності серед своєї категорії людей.
+//         </div>
+//         <div className="about-us__text-italic">
+//           Окрема увага приділяється якості: всі моделі бренду Khaby
+//           виготовляються з якісних тканин на українських фабриках.
+//         </div>
+//         <div className="about-us__text">
+//           Ключовими моделями колекцій Khaby є брючні костюми, сорочки, спідниці
+//           та брюки з бездоганною посадкою, а також сукні та светри, які
+//           вписуються в контекст будь-якого гардеробу і, зберігають свою
+//           актуальність протягом кількох сезонів.
+//           <br /> Наша ідея - допомогти дівчатам створити практичний і продуманий
+//           базовий гардероб!
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+const PageAboutUsImg = () => {
+  const [data, setData] = useState(false);
+  if (!data) {
+    fetch(wordPressAPIUrl + "/wp/v2/pages/103")
+      .then((response) => {
+        return response.json();
+      })
+      .then((respData) => {
+        let content = respData.content.rendered;
+        setData(content);
+      });
+  }
+  return <>{data && <div dangerouslySetInnerHTML={{ __html: data }}></div>}</>;
+};
+
+const PageAboutUsText = () => {
+  const [data, setData] = useState(false);
+  if (!data) {
+    fetch(wordPressAPIUrl + "/wp/v2/pages/53")
+      .then((response) => {
+        return response.json();
+      })
+      .then((respData) => {
+        let content = respData.content.rendered;
+        setData(content);
+      });
+  }
+  return <>{data && <div dangerouslySetInnerHTML={{ __html: data }}></div>}</>;
+};
+
+export function PageAboutUs() {
+  return (
+    <div className="container">
+      {" "}
+      <div className="about-us__image-box">
+        <PageAboutUsImg />
+      </div>
+      <div className="about-us__info-box about-us__text">
+        {" "}
+        <PageAboutUsText />
+      </div>
+    </div>
+  );
 }
